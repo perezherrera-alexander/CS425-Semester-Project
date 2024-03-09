3e57957661acfa06058ed8f304e485cd30558074
perezherrera-alexander <aperezherrera@nevada.unr.edu>
2024-03-08 16:27:53 -0800

    Created 3 new paths and decorated path 02
    Created three new paths (04, 05, & 06) and added them to the pool of paths. Also decorated path 02 with a desert theme, complete with two types of cacti (properly tagged and collidered)

--- Assets/Scripts/Level Logic/PathLoading.cs
+++ Assets/Scripts/Level Logic/PathLoading.cs
@@ -5,26 +5,36 @@ using UnityEngine;
 
 public class PathLoading : MonoBehaviour, ISaveable
 {
-    [Tooltip("Enable to use debug mode, which will not load a saved or random level, but use the one already in the scene.")]
+    [Tooltip("Enable to use debug mode, which will not load a specified, saved, or randomly chosen level (use the one already in the scene).")]
     public bool enableDebug = false;
+    [Tooltip("Enable to only load the level specified by the index. Will ignore saved level or rnadom choice (but not debug mode).")]
+    public bool loadSpecificLevel = false;
+    public int specificLevelIndex = 0;
     public GameObject[] pathPrefabs;
     public PlayerData playerData;
-    public int LevelChoice = 0;
+    public int LevelChoice = 0; // I feel like this can private
     void Start()
     {
-        
         if(enableDebug) return; // If debug is enabled, don't load saved level or randomly pick one (use the one already in the scene)
+
         // If "World Objects" has a child, destroy it
         if (transform.childCount > 0)
         {
             Destroy(transform.GetChild(0).gameObject);
+        }
+        if(loadSpecificLevel) // Load the specified level, ignoring everything else (debug mode, save data, random choice)
+        {
+            GameObject path = Instantiate(pathPrefabs[specificLevelIndex], transform.position, Quaternion.identity);
+            path.transform.parent = transform;
+            assignSTARTpoint(path);
+            return;
         }
 
         // If there is save data, load the level from the save data, otherwise load a random level
         if (playerData.LevelLoaded == false)
         {
             // Assign one of the prefab paths to the "World Objects" game object
-            int pathIndex = UnityEngine.Random.Range(0, pathPrefabs.Length);
+            int pathIndex = UnityEngine.Random.Range(1, pathPrefabs.Length);
             LevelChoice = pathIndex;
             GameObject path = Instantiate(pathPrefabs[pathIndex], transform.position, Quaternion.identity);
             path.transform.parent = transform;
@@ -44,6 +54,13 @@ public class PathLoading : MonoBehaviour, ISaveable
 
             playerData.LevelLoaded = false;
         }
+    }
+
+    public void assignSTARTpoint(GameObject pathPrefab)
+    {
+        //Debug.Log("Assigning START point");
+        GameObject waveSpawner = GameObject.Find("Game Master");
+        waveSpawner.GetComponent<WaveSpawner>().SpawnPoint = pathPrefab.transform.GetChild(1);
     }
 
     public object CaptureState()
