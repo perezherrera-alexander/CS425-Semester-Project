d6260eb8bcf47ff9921013f8a54b98b1a8174ed3
grubio1 <70547757+Gustavo-Rubio@users.noreply.github.com>
2024-03-19 18:34:56 -0700

    Uninheritable fast robot
    [serializefield]

--- Assets/Scripts/Enemy/Fast Enemy.cs
+++ Assets/Scripts/Enemy/Fast Enemy.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class FastEnemy : BaseEnemyLogic
 {
-    public override void Update(){
+    public override void Update (){
         healthCheck();
-        effect_check();
+
+        //if(effects.Count > 0)
+        //{
+        //    if (effects.First() != null) handleEffect();
+        //}
+        
         Vector3 direction = target.position - transform.position;
-        transform.Translate(direction.normalized * speed * 2 * Time.deltaTime, Space.World);
+
+        //Enemy speeds up depending on the damage taken
+        transform.Translate(direction.normalized * speed * Time.deltaTime * Math.Min(5, 1/(health/maxHealth)), Space.World);
 
         if (Vector3.Distance(transform.position, target.position) <= 0.4f){
             GetNextWaypoint();
