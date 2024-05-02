4f22a4dde835cae1a8082e619f9c63f25ba60033
grubio1 <70547757+Gustavo-Rubio@users.noreply.github.com>
2024-05-02 12:58:40 -0700

    Writer for later

--- Assets/Scripts/UI/TextWriter.cs
+++ Assets/Scripts/UI/TextWriter.cs
@@ -6,24 +6,81 @@ using TMPro;
 
 public class TextWriter : MonoBehaviour
 {
-    private TextMeshProUGUI uiText;
-    private string message;
-    private int characterIndex;
-    private float speed;
-    private float timer;
-    private bool invisible;
-    public void AddText(TextMeshProUGUI text, string message, float speed, bool invisible)
-    {
-        this.uiText = text;
-        this.message = message;
-        this.speed = speed;
-        this.invisible = invisible;
+    public static TextWriter Instance { get; private set; }
+    private List<TextWriterSingle> textWriterSingleList;
 
-        characterIndex = 0;
+    private void Awake()
+    {
+        textWriterSingleList = new List<TextWriterSingle>();
+        Instance = this;
+    }
+    public TextWriterSingle AddText(TextMeshProUGUI text, string message, float speed, bool invisible)
+    {
+        TextWriterSingle textWriterSingle = new TextWriterSingle(text, message, speed, invisible);
+        textWriterSingleList.Add(textWriterSingle);
+        return textWriterSingle;
+    }
+    public static TextWriterSingle AddText_Static(TextMeshProUGUI text, string message, float speed, bool invisible, bool removeWriter)
+    {
+        if (removeWriter)
+        {
+            Instance.RemoveWriter(text);
+        }
+        return Instance.AddText(text, message, speed, invisible);
+
     }
     private void Update()
     {
-        if (uiText!= null)
+        //Debug.Log("textWriterSingleList.Count: " + textWriterSingleList.Count);
+        for (int i = 0; i < textWriterSingleList.Count; i++)
+        {
+            bool destroyInstance = false;
+            textWriterSingleList[i].Update();
+            if (textWriterSingleList[i] == null)
+            {
+                destroyInstance = true;
+            }
+            if (destroyInstance)
+            {
+                textWriterSingleList.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+    private void RemoveWriter(TextMeshProUGUI uiText)
+    {
+        for (int i = 0; i < textWriterSingleList.Count; i++)
+        {
+            if (textWriterSingleList[i].GetUIText() == uiText)
+            {
+                textWriterSingleList.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    public static void RemoveWriter_Static(TextMeshProUGUI uiText)
+    {
+        Instance.RemoveWriter(uiText);
+    }
+
+    public class TextWriterSingle{
+        private TextMeshProUGUI uiText;
+        private string message;
+        private int characterIndex;
+        private float speed;
+        private float timer;
+        private bool invisible;
+        public TextWriterSingle(TextMeshProUGUI text, string message, float speed, bool invisible)
+        {
+            this.uiText = text;
+            this.message = message;
+            this.speed = speed;
+            this.invisible = invisible;
+
+            characterIndex = 0;
+        }
+        public bool Update()
         {
             timer -= Time.deltaTime;
             //int characters = (int)timer;
@@ -42,10 +99,26 @@ public class TextWriter : MonoBehaviour
                 if (characterIndex >= message.Length)
                 {
                     uiText = null;
-                    return;
+                    return true;
                 }
             }
-            
+            return false;
+        }
+
+        public TextMeshProUGUI GetUIText()
+        {
+            return uiText;
+        }
+
+        public bool IsActive()
+        {
+            return uiText != null;
+        }
+
+        public void WriteDestroy(){
+            uiText.text = message;
+            TextWriter.RemoveWriter_Static(uiText);
+            characterIndex = message.Length;
         }
     }
 }
--- Assets/Scripts/UI/UI_Assistant.cs
+++ Assets/Scripts/UI/UI_Assistant.cs
@@ -2,18 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 // using UnityEngine.UI;
-// using TMPro;
+using TMPro;
 
 public class UI_Assistant : MonoBehaviour
 {
-    [SerializeField] private PartOne partone;
+    private TextMeshProUGUI messageText;
+    private TextWriter.TextWriterSingle textWriterSingle;
+    
     private void Awake(){
-        partone = transform.Find("PartOne").GetComponent<PartOne>();
-        //enable partone script after 5 seconds
-        Invoke("EnablePartOne", 5f);
-        //Application.targetFrameRate = 60;
+        messageText = transform.Find("Message").GetComponent<TextMeshProUGUI>();
+        //  transform.Find("Message").getComponent<Button_UI>().ClickFunc = () => {
+            if (textWriterSingle != null && textWriterSingle.IsActive()){
+                textWriterSingle.WriteDestroy();
+            }else{
+                string[] msgArr = new string[]{
+                    "Hello, I am your assistant. I will guide you through the game.",
+                    "You should really use shooter towers on straight lines to hit multiple enemies",
+                    "Don't forget to use towers with special abilities",
+                    "You can upgrade your towers by clicking spending tokens earned from beating maps",
+                    "You can also sell towers for some of the price",
+                    "Good luck!"
+                };
+                string msg = msgArr[Random.Range(0, msgArr.Length)];
+                textWriterSingle = TextWriter.AddText_Static(messageText, msg, .1f, true, true);
+            };
+        // };
     }
-    private void EnablePartOne(){
-        partone.enabled = true;
+
+    private void Start(){
+        TextWriter.AddText_Static(messageText, "Hello, I am your assistant. I will guide you through the game.", .1f, true, true);
     }
+    // private void Awake(){
+    //     partone = transform.Find("PartOne").GetComponent<PartOne>();
+    //     //enable partone script after 5 seconds
+    //     Invoke("EnablePartOne", 5f);
+    //     //Application.targetFrameRate = 60;
+    // }
+    // private void EnablePartOne(){
+    //     partone.enabled = true;
+    // }
 }
